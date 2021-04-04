037d7513f500a751e7b8d250f80b5d829a3df711
Temnikova Tatiana <tatiana.s.kalugina@gmail.com>
2021-04-04 22:30:04 +0200

    Change test structure to PageObject notation

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+LINK = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(LINK)
+    go_to_login_page(browser)
