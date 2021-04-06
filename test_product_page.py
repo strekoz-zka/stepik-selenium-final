61ea815429017a5145debe84102f438f0dda6dcc
Temnikova Tatiana <tatiana.s.kalugina@gmail.com>
2021-04-06 23:20:19 +0200

    Rearrange methods in alphabetical order. Move login page methods to BasePage class. Add tests for login from product page.

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
 
 
@@ -39,3 +40,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_cart()
     page.should_success_message_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
